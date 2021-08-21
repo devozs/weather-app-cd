c5a0ac3ad909d4b66a5478810448c6bbd117f9c8
DevOps Versioning <devops@datomize.com>
2021-08-21 23:10:21 +0300

    Update readme

--- verify-deployment.sh
+++ verify-deployment.sh
@@ -2,7 +2,7 @@
 
 POD_NAME=$(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{end}}' | grep weather-app)
 POD_LOGS=$(kubectl logs "${POD_NAME}")
-echo "${POD_LOGS}"
+kubectl logs "${POD_NAME}"
 
 if [[ ${POD_LOGS} == *"Response Code: 200"* ]]; then
   echo 'Verification finished successfully'
